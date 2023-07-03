892db8ab05e5a249c4a097d0d9deaca1af82eebd
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2023-07-03 19:33:30 +0200

    AutoFilter and Eq8 presets finetuned

---
+++ preloaded/AutoFilter.lua
@@ -0,0 +1,87 @@
+ftype = 0
+ctype1 = 0
+ctype2 = 0
+
+
+circuit1 = controls.get(7)
+circuit2 = controls.get(6)
+morph = controls.get(21)
+drive = controls.get(2)
+
+function setfiltervisibility ()
+   circuit1:setVisible(ftype < 2)
+   circuit2:setVisible(ftype > 1)
+   morph:setVisible(ftype == 4)
+   drive:setVisible( ((ftype == 2 or ftype == 3) and (ctype2 ~=0)) or ((ftype < 2) and (ctype1 ~= 0)))
+end
+
+function setftype (valueObject, value)
+    ftype = value
+    setfiltervisibility()
+end
+
+function setctype1 (valueObject, value)
+    ctype1 = value
+    setfiltervisibility()
+end
+
+function setctype2 (valueObject, value)
+    ctype2 = value
+    setfiltervisibility()
+end
+
+quantize = controls.get(15)
+
+function qon (valueObject, value)
+   if value == 0 then
+      quantize:setVisible(false)
+   else
+      quantize:setVisible(true)   
+   end
+end
+
+scison = false
+
+scgain = controls.get(23)
+scmix = controls.get(24)
+
+function setscvisibility()
+    scgain:setVisible(scison)
+    scmix:setVisible(scison)
+end
+
+function scon(valueObject, value)
+    scison = (value ~= 0)
+    setscvisibility()
+end
+
+syncison = false
+
+lfofreq = controls.get(11)
+lforate = controls.get(19)
+lfooffset = controls.get(12)
+lfostereomode = controls.get(17)
+
+spinison = false
+
+lfospin = controls.get(16)
+lfophase = controls.get(13)
+
+function setlfovisibility()
+  lfofreq:setVisible(not syncison)
+  lforate:setVisible(syncison)
+  lfooffset:setVisible(syncison)
+  lfostereomode:setVisible(not syncison)
+  lfospin:setVisible(spinison)
+  lfophase:setVisible(syncison or (not spinison))
+end
+
+function sync (valueObject, value)
+    syncison = (value ~= 0)
+    setlfovisibility()
+end
+
+function stereomode (valueObject, value)
+    spinison = (value ~= 0)
+    setlfovisibility()
+end
