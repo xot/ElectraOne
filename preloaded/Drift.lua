426b20df68b89d9f692a0811f7c44b8435de6558
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2024-01-11 23:02:36 +0100

    Drift preset improved

--- preloaded/Drift.lua
+++ preloaded/Drift.lua
@@ -9,25 +9,45 @@ lfosynced:setSlot(1,2)
 
 function lfomode(valueObject, value)
     lforate:setVisible(value == 0)
-    lfotime:setVisible(value == 1)
+    lfotime:setVisible(value == 1) 
     lforatio:setVisible(value == 2)
     lfosynced:setVisible(value == 3)
 end
 
-envrate = controls.get(2)
-envtime = controls.get(6)
-envratio = controls.get(3)
-envsynced = controls.get(4)
+env2rate = controls.get(2)
+env2time = controls.get(6)
+env2ratio = controls.get(3)
+env2synced = controls.get(4)
 
-envtime:setSlot(36,1)
-envratio:setSlot(36,1)
-envsynced:setSlot(36,1)
+env2time:setSlot(36,1)
+env2ratio:setSlot(36,1)
+env2synced:setSlot(36,1)
 
+env2 = controls.get(78)
+env2cycmode = controls.get(7)
+env2tilt = controls.get(5)
+env2hold = controls.get(1)
 
-function envmode(valueObject, value)
-    print(value)
-    envrate:setVisible(value == 0.0)
-    envtime:setVisible(value == 1.0)
-    envratio:setVisible(value == 2.0)
-    envsynced:setVisible(value == 3.0)
+env2cycle = false
+env2mode = 0
+
+function env2setvisibility()
+    env2rate:setVisible(env2mode == 0.0 and env2cyc)
+    env2time:setVisible(env2mode == 1.0 and env2cyc)
+    env2ratio:setVisible(env2mode == 2.0 and env2cyc)
+    env2synced:setVisible(env2mode == 3.0 and env2cyc)
+    env2:setVisible(not env2cyc)
+    env2cycmode:setVisible(env2cyc)
+    env2tilt:setVisible(env2cyc)
+    env2hold:setVisible(env2cyc)
+end
+
+function setenv2mode(valueObject, value)
+    env2mode = value
+    env2setvisibility()
+end
+
+function setenv2cycle(valueObject, value)
+    env2cyc = (value ~= 0.0)
+    env2setvisibility()
 end
