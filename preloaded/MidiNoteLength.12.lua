1020ba31ac3e839c1b1bb4461f4fb2b5bb1f18c2
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2024-03-06 22:09:55 +0100

    MidiNoteLength for Live 12 improved

--- preloaded/MidiNoteLength.12.lua
+++ preloaded/MidiNoteLength.12.lua
@@ -1,19 +1,36 @@
 release = controls.get(6)
 decay = controls.get(2)
 keyscale = controls.get(1)
-
-function mode(valueObject, value)
-    release:setVisible(value ~= 0)
-    decay:setVisible(value ~= 0)
-    keyscale:setVisible(value ~= 0)   
-end
-
 timelength = controls.get(9)
 synclength = controls.get(8)
+gate = controls.get(4)
 
 synclength:setSlot(9,1)
 
+modestate = false
+syncstate = false
+latchstate = false
+
+function mode(valueObject, value)
+    modestate = (value ~= 0)
+    setvisibility()
+end
+
 function sync(valueObject, value)
-    timelength:setVisible(value == 0)
-    synclength:setVisible(value ~= 0)
+    syncstate = (value ~= 0)
+    setvisibility()
+end
+
+function latch(valueObject, value)
+    latchstate = (value ~= 0)
+    setvisibility()
+end
+
+function setvisibility()
+    release:setVisible(not latchstate and not modestate)
+    decay:setVisible(not latchstate and not modestate)
+    keyscale:setVisible(not latchstate and not modestate)  
+    gate:setVisible(not latchstate)
+    timelength:setVisible(not latchstate and not syncstate)  
+    synclength:setVisible(not latchstate and syncstate)
 end
