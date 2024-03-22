534019c37a645f64b4ba4db5481adb5c73f4b976
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2024-03-22 11:51:04 +0100

    Collision preset redesigned; related controls in same display section

--- preloaded/Collision.lua
+++ preloaded/Collision.lua
@@ -4,8 +4,8 @@ freq1 = controls.get(14)
 rate2 = controls.get(33)
 freq2 = controls.get(28)
 
-rate1:setSlot(19,4)
-rate2:setSlot(22,4)
+rate1:setSlot(4,4)
+rate2:setSlot(28,4)
 
 function sync1(valueObject, value)
     freq1:setVisible(value == 0)
@@ -22,8 +22,9 @@ inharmonics1 = controls.get(78)
 radius1 = controls.get(93)
 opening1 =  controls.get(87)
 
-radius1:setSlot(8,2)
-opening1:setSlot(14,2)
+
+radius1:setSlot(5,2)
+opening1:setSlot(8,2)
 
 function type1(valueObject, value)
     material1:setVisible(value < 5)
@@ -37,8 +38,8 @@ inharmonics2 = controls.get(109)
 radius2 = controls.get(124)
 opening2 =  controls.get(118)
 
-radius2:setSlot(8,3)
-opening2:setSlot(14,3)
+radius2:setSlot(5,3)
+opening2:setSlot(8,3)
 
 function type2(valueObject, value)
     material2:setVisible(value < 5)
