d62fc35b5535615958d57d342444385c0db1e32d
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2024-10-11 18:26:52 +0200

    Added 12.1 version of Limiter; fixed Midi*.ccmap for 12.1

---
+++ preloaded/Limiter.12.1.lua
@@ -0,0 +1,26 @@
+gain = controls.get(4)
+output = controls.get(9)
+
+output:setSlot(1,1)
+
+ceiling = controls.get(2)
+threshold = controls.get(10)
+
+threshold:setSlot(3,1)
+
+lrlink = controls.get(13)
+mslink = controls.get(12)
+
+mslink:setSlot(4,1)
+
+function dorouting(valueObject, value)
+    lrlink:setVisible(value == 0)
+    mslink:setVisible(value ~= 0)    
+end
+
+function domaximize(valueObject, value)
+    gain:setVisible(value == 0)
+    output:setVisible(value ~= 0)
+    threshold:setVisible(value == 0)
+    ceiling:setVisible(value ~= 0)
+end
