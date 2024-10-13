8a2560cdb3c8d3c5f82d65a74dc383be18564983
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2024-10-14 00:01:36 +0200

    Added VectorDelay for real :-/

---
+++ preloaded/VectorDelay.lua
@@ -0,0 +1,19 @@
+freq = controls.get(54)
+sync = controls.get(56)
+
+sync:setSlot(8,1)
+
+function dosync(valueObject, value)
+    freq:setVisible(value == 0)
+    sync:setVisible(value ~= 0)    
+end
+
+filter = controls.get(20)
+ladder = controls.get(29)
+
+ladder:setSlot(6,1)
+
+function doladder(valueObject, value)
+    filter:setVisible(value == 0)
+    ladder:setVisible(value ~= 0)    
+end
